31ccd82123f4f1713557cc8223c6c57cb5d00d83
Trinity Song <kyeongyeon.song@gmail.com>
2016-07-25 23:05:41 +0900

    Refector API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;  //Retrive port number used on heroku or use 3000
@@ -20,7 +21,8 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req,res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchdTodo;
+
+/*	var matchdTodo;
 	//Iterate over todos array. Find the match.
 
 	todos.forEach(function(todo) {
@@ -28,23 +30,32 @@ app.get('/todos/:id', function(req,res) {
 			matchdTodo = todo;
 		}
 	});
+*/
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if(matchdTodo)
-		res.json(matchdTodo);
+	if(matchedTodo)
+		res.json(matchedTodo);
 	else
 		res.status(404).send();
 
 	//res.send('Asking for todo with id of ' + req.params.id)
 });
 
-//POST /todos
+//POST /todos(
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	var body = req.body; //use _.pick to only pick description and completed.
+	var body = _.pick(req.body, 'description','completed');
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	// set body.description to be trimmed value
+	body.description = body.description.trim();
 
 	//ADD id field
 	body.id = todoNextId;
 	todoNextId = todoNextId + 1;
-	
+
 	//push body into array
 	todos.push(body);
 
