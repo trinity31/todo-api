2b71eeb31591596444d79542c5e81b8cfe7a3d73
Trinity Song <kyeongyeon.song@gmail.com>
2016-07-26 21:53:41 +0900

    final todos static

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
 	res.send('TODO API Root');
 });
 
-//GET /todos
+//GET /todos?completed=false&q=work
 app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -22,6 +22,13 @@ app.get('/todos', function(req, res) {
 		filteredTodos = _.where(filteredTodos, {completed:true});
 	} else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed:false});
+	}
+
+	//"Go to work on Saturday.".indexOf('work')
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 
 	res.json(filteredTodos);
