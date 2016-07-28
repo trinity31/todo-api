d8443840ad28a7499f79a2854c8713a8f44d0c40
Trinity Song <kyeongyeon.song@gmail.com>
2016-07-28 22:10:57 +0900

    Add sequelize demo

---
+++ playground/basic-sqlite-database.js
@@ -0,0 +1,79 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+	'dialect': 'sqlite',
+	'storage': __dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: [1, 250]
+		}
+	},
+	completed: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+	}
+})
+
+sequelize.sync({
+	force:true
+}).then(function() {
+	console.log('Everything is synced');
+
+	Todo.create({
+		description: 'Feed the cat',
+		completed: true
+	}).then(function(todo) {
+		return Todo.create({
+			description: 'Wash the dishes',
+			completed: true
+		});
+	}).then(function() {
+		return Todo.findById(1);
+	}).then(function(todo) {
+		if(todo){
+			console.log(todo.toJSON());
+		} else {
+			console.log('No todo item found');
+		}
+	}).catch(function(e) {
+		console.log(e);
+	});
+	
+	/*;
+	Todo.create({
+		description: 'Take out trash',
+		//completed: false
+	}).then(function(todo) {
+		//console.log('Finished!');
+		//console.log(todo);
+		return Todo.create({
+			description: 'Clean office'
+		});
+	}).then(function() {
+			//return Todo.findById(1);
+			return Todo.findAll({
+				where: {
+					description: {
+						$like: '%Office%'
+					}
+				}
+			});
+	}).then(function(todos) {
+		if(todos) {
+			todos.forEach(function(todo) {
+				console.log(todo.toJSON());
+			});
+			//console.log(todo.toJSON());
+		} else {
+			console.log('No todo found');
+		}
+	}).catch(function(e) {
+		console.log(e);
+	}); */
+});
+
