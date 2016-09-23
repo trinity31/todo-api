54700cd588945c78b3809e28b2918a9564bffd9f
Trinity Song <kyeongyeon.song@gmail.com>
2016-09-23 22:37:44 +0900

    Add auth middleware and findByToken class method

--- models/user.js
+++ models/user.js
@@ -64,6 +64,28 @@ module.exports = function(sequelize, DataTypes) {
 						reject();
 					});	
 				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodeJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodeJWT.token, 'abc123!@!');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8))
+
+
+						user.findById(tokenData.id).then(function(user) {
+							if(user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function(e) {
+							reject();	
+						});
+					} catch(e) {
+						reject();
+					}
+				});
 			}
 		},
 		instanceMethods: {
